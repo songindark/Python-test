4ac3dade662e85ebe5637ae42b3d6ae27e188eeb
songindark <songindark@qq.com>
2020-12-29 14:43:28 +0800

    change code

--- test/FB.py
+++ test/FB.py
@@ -1,14 +1,15 @@
-def fib(n):    # ���嵽 n ��쳲���������
+def fib(n):    # 定义到 n 的斐波那契数列
     a, b = 0, 1
     while b < n:
-        print(b, end=' ')    # end=' '��˼��ĩβ�����У��ӿո�
+        print(b, end=' ')    # end=' '意思是末尾不换行，加空格
         a, b = b, a+b
     print()
  
-def fib2(n): # ���ص� n ��쳲���������
+def fib2(n): # 返回到 n 的斐波那契数列
     result = []
     a, b = 0, 1
     while b < n:
         result.append(b)
         a, b = b, a+b
     return result
+print(fib(100))
